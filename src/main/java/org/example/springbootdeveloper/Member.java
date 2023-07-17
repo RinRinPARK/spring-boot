007aa9a216a99d6e8e0a374d2d603079ab4c58de
RInRInPark <surinp8019@naver.com>
2023-07-18 00:18:51 +0900

    Chapter 5: ORM

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -6,13 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // 엔티티로 지정, Member클래스와 실제 DB테이블을 매핑시킨다. -> member이름을 가진 테이블과 매핑
+//@Entity(name = "member_list") -> member_list라는 이름을 가진 테이블과 매핑하고 싶다면 이렇게!
 public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY는 AUTO_INCREMENT와 뜻이 같다.
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 'id' 컬럼과 매칭
 
