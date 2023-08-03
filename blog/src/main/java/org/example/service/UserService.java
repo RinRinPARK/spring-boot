595b07750236f4d5f0377cdac4f4e6e53cb458df
RInRInPark <surinp8019@naver.com>
2023-08-03 14:19:28 +0900

    Chatper 8: Spring Security

---
+++ blog/src/main/java/org/example/service/UserService.java
@@ -0,0 +1,24 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.domain.User;
+import org.example.dto.AddUserRequest;
+import org.example.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+}
